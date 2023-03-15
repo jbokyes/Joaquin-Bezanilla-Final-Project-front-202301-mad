9b79bdd3b0a5cf18a0aeb8eeec646be7aa39cfee
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-15 22:29:21 +0100

    Second attempt to fix app-router test

--- src/components/app-router/app-router.tsx
+++ src/components/app-router/app-router.tsx
@@ -1,31 +1,28 @@
 import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import About from "../about/about";
-import { MenuOption } from "../app/app";
+import { MenuOption, menuOptions } from "../app/app";
 import Favourites from "../favourites/favourites";
 import Login from "../login/login";
 import Register from "../register/register";
 
 const Home = lazy(() => import("../home/home"));
 
-type AppRouterProps = {
-  menuOptions: MenuOption[];
-};
-
-export function AppRouter({ menuOptions }: AppRouterProps) {
+export function AppRouter() {
+  const navMenuOptions: MenuOption[] = menuOptions;
   return (
     <Suspense>
       <Routes>
         <Route path={"/"} element={<Home></Home>}></Route>
-        <Route path={menuOptions[0].path} element={<Home></Home>}></Route>
-        <Route path={menuOptions[1].path} element={<About></About>}></Route>
-        <Route path={menuOptions[2].path} element={<Login></Login>}></Route>
+        <Route path={navMenuOptions[0].path} element={<Home></Home>}></Route>
+        <Route path={navMenuOptions[1].path} element={<About></About>}></Route>
+        <Route path={navMenuOptions[2].path} element={<Login></Login>}></Route>
         <Route
-          path={menuOptions[3].path}
+          path={navMenuOptions[3].path}
           element={<Register></Register>}
         ></Route>
         <Route
-          path={menuOptions[4].path}
+          path={navMenuOptions[4].path}
           element={<Favourites></Favourites>}
         ></Route>
       </Routes>
