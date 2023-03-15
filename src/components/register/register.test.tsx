1a9ac46c8139b7041092a7a9f66100a5d49f46d7
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-15 21:42:38 +0100

    Attempt 1 to fix sonar

--- src/components/register/register.test.tsx
+++ src/components/register/register.test.tsx
@@ -1,10 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../store/store";
 import { Register } from "./register";
 
 describe("Given the Home component", () => {
   describe("When rendering", () => {
     test("then it should render Register", () => {
-      render(<Register></Register>);
+      render(
+        <Provider store={store}>
+          <Register></Register>
+        </Provider>
+      );
       const element = screen.getByRole("heading");
       expect(element).toBeInTheDocument();
     });
