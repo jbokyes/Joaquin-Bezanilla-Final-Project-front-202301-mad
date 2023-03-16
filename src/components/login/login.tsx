19b19c207d28efd77c1ee44bbcf49db093e98100
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-16 12:05:49 +0100

    Add login component test

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+/* eslint-disable jsx-a11y/no-redundant-roles */
+import { SyntheticEvent, useMemo } from "react";
+import { useUsers } from "../../hooks/use.users";
+import { UserStructure } from "../../models/user";
+import { UsersApiRepo } from "../../services/repositories/users.api.repo";
 
 export function Login() {
-  return <h2>Login</h2>;
+  const userRepo = useMemo(() => new UsersApiRepo(), []);
+
+  const { loginUser } = useUsers(userRepo);
+
+  const handleSubmit = (ev: SyntheticEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+
+    const formLoginUser = ev.currentTarget;
+
+    const logUser: Partial<UserStructure> = {
+      email: (formLoginUser.elements[0] as HTMLFormElement).value,
+      passwd: (formLoginUser.elements[1] as HTMLFormElement).value,
+    };
+
+    loginUser(logUser);
+
+    formLoginUser.reset();
+  };
+  return (
+    <div className="login">
+      <h2>Login</h2>
+      <p>Type in your registered credentials!</p>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="email" placeholder="Email" required />
+        <input
+          type="password"
+          name="password"
+          role="textbox"
+          placeholder="Password:"
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
