092a006bfee480258bd9efc689fd6342bd209c1e
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-16 19:52:44 +0100

    Add .env files

--- src/components/firebase/firebase-config.ts
+++ src/components/firebase/firebase-config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBoHCJMhxhrd5G85sl9m2feItB14q7lJ9A",
+  apiKey: process.env.FB_APIKEY,
   authDomain: "latino-food-9e36e.firebaseapp.com",
   projectId: "latino-food-9e36e",
   storageBucket: "latino-food-9e36e.appspot.com",
-  messagingSenderId: "307612411906",
-  appId: "1:307612411906:web:b986c4fea02572c92d1f1b",
+  messagingSenderId: process.env.FB_MSID,
+  appId: process.env.FB_APPID,
+  measurementId: process.env.FB_MEASUREID,
 };
