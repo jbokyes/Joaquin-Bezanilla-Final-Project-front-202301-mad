287592007609f3f0008e65341ffa2603c56433fa
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-16 15:25:48 +0100

    Add login css

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -4,6 +4,8 @@ import { useUsers } from "../../hooks/use.users";
 import { UserStructure } from "../../models/user";
 import { UsersApiRepo } from "../../services/repositories/users.api.repo";
 
+import styles from "./login.module.scss";
+
 export function Login() {
   const userRepo = useMemo(() => new UsersApiRepo(), []);
 
@@ -24,9 +26,9 @@ export function Login() {
     formLoginUser.reset();
   };
   return (
-    <div className="login">
+    <div className={styles.login}>
       <h2>Login</h2>
-      <p>Type in your registered credentials!</p>
+      <p className="type-in">Type in your registered credentials.</p>
 
       <form onSubmit={handleSubmit}>
         <input type="text" name="email" placeholder="Email" required />
