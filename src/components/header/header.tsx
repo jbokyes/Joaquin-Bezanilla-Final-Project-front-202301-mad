ac267427c9f810c0a4f610ea893042227ebeb813
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-19 20:06:25 +0100

    add header positioning

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-
+import styles from "./header.module.scss";
 type HeaderProps = { children: JSX.Element };
 
 export function Header({ children }: HeaderProps) {
   return (
-    <header>
-      <h1 className="header__title">Actual latino foods</h1>
+    <header className={styles.header}>
+      <img
+        className={styles.header__image}
+        src="./images/sopaipilla-kawaii.jpg"
+        alt="sopaipilla-kawaii"
+      />
+      <h1 className={styles.header__title}>Actual latino foods</h1>
       <div>{children}</div>
     </header>
   );
