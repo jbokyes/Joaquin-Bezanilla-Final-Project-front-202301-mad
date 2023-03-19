2d301b5a776f2cb071c8877ebd6451f692ad1a51
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-19 14:37:35 +0100

    Add readId and update to users repo test

--- src/services/repositories/users.repo.test.ts
+++ src/services/repositories/users.repo.test.ts
@@ -34,4 +34,42 @@ describe("Given UsersApiRepo class being instanced", () => {
       await expect(result).rejects.toThrow();
     });
   });
+  describe("When the readId method is called", () => {
+    test("Then if the fetch response is ok, the result should be equal to the mock value", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue({
+          username: "joaquín",
+        }),
+      });
+      const result = await mockRepoUsers.readId("testId", "tokenTest");
+      await expect(result).toEqual({ username: "joaquín" });
+    });
+    test("Then if the fetch response is not ok, it should throw us an error", async () => {
+      global.fetch = jest.fn().mockResolvedValue("Error");
+      const result = mockRepoUsers.readId("testId", "testToken");
+      await expect(result).rejects.toThrow();
+    });
+  });
+  describe("When we call the update method", () => {
+    test("Then if the fetch response is ok, the result should equal the mock value", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue({
+          username: "joaquín",
+        }),
+      });
+      const result = await mockRepoUsers.update(
+        "testId",
+        "testToken",
+        "testAction"
+      );
+      expect(result).toEqual({ username: "joaquín" });
+    });
+    test("Then if the fetch response is not ok, it should throw us an error", async () => {
+      global.fetch = jest.fn().mockResolvedValue("test error");
+      const result = mockRepoUsers.update("test", "test", "test");
+      await expect(result).rejects.toThrowError();
+    });
+  });
 });
