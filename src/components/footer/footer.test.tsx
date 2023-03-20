e01f3aad3f79aa7452433b0dbc6ff817cc03317b
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-20 22:07:00 +0100

    Fix sonar footer test

--- src/components/footer/footer.test.tsx
+++ src/components/footer/footer.test.tsx
@@ -3,10 +3,10 @@ import { Footer } from "./footer";
 
 describe("Given the Footer component", () => {
   describe("When rendering", () => {
-    test("then it should render Home", () => {
+    test("then it should render Footer", () => {
       render(<Footer></Footer>);
-      const element = screen.getByRole("img");
-      expect(element).toBeInTheDocument();
+      const element = screen.getAllByRole("img");
+      expect(element[0]).toBeInTheDocument();
     });
   });
 });
